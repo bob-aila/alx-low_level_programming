da4b1db23d3defa335c82f85029287b992677035
bob-aila <61519243+bob-aila@users.noreply.github.com>
2023-03-08 22:27:47 +0300

    first doc

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,7 +1,19 @@
-void _puts_recursion(char *s);
-int _putchar(char c);
-void _print_rev_recursion(char *k);
-int _strlen_recursion(char *k);
-int factorial(int g);
-int _pow_recursion(int x, int y);
+#include "main.h"
 
+/**
+ * _puts_recursion - Prints a string followed by a new line
+ * @s: string
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == 0)
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(*s);
+	_puts_recursion(s + 1);
+}
