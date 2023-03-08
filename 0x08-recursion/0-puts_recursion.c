fbdd4268324f5183c8a6387fdf50e7c43d31c504
Bob <bobphilip93@gmail.com>
2023-03-08 11:08:13 -0800

    first

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,7 @@
+void _puts_recursion(char *s);
+int _putchar(char c);
+void _print_rev_recursion(char *k);
+int _strlen_recursion(char *k);
+int factorial(int g);
+int _pow_recursion(int x, int y);
+
