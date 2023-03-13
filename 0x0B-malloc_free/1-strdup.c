6d839090480afd65ddff130be61bc6977a6b367f
bob-aila <61519243+bob-aila@users.noreply.github.com>
2023-03-13 22:25:31 +0300

    mallocs

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,46 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - a function to copy a string on a new allocated memory area .
+ * @str: string to copy
+ *
+ * Return: a pointer to the copied string .
+ */
+
+char *_strdup(char *str)
+{
+char *pointeur;
+int i;
+int count = 0;
+
+/* if the given string is NULL*/
+if (str == NULL)
+	return (NULL);
+
+/* to finf the length of the given string*/
+while (*(str + count))
+
+	count++;
+/*allocating needed dynamic memory*/
+pointeur = malloc(sizeof(char) * (count + 1));
+
+
+/* verifying if the pointer , pointed on the allocated memory area is not NULL */
+if (pointeur == NULL)
+	return (NULL);
+
+/* initializing i to avoid segment fault error*/
+i = 0;
+
+/* filling the allocated area with the bytes from the given string */
+while (i <= count)
+{
+	*(pointeur + i) = *(str + i);
+		i++;
+}
+
+/* return a pointer to the string */
+return (pointeur);
+}
+
